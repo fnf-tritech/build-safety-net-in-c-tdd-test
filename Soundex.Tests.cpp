bf2069cb12081f33e88dfc6c312bdef951f5bae6
Venugopal K <venu.shastri@gmail.com>
2024-09-12 16:44:55 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -1,2 +1,10 @@
 #include <gtest/gtest.h>
 #include "Soundex.h"
+
+
+TEST(SoudexAlgorithmSuite,generateSoundex_given_emptyString_expected_emptyString){
+
+  char soudexCode[5];
+  generateSoudex("",soundexCode);
+  ASSERT_STREQ(soudexCode, "");
+}
