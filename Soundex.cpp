d973429e31c22d01a65890ef80e36bae120ec38d
Venugopal K <venu.shastri@gmail.com>
2024-09-12 16:58:06 +0530

    Update Soundex.cpp

--- Soundex.cpp
+++ Soundex.cpp
@@ -1,7 +1,14 @@
 #include "Soundex.h"
-
+int checkForEmptyInput(const char* input){
+    if (input[0] == '\0')
+    {
+     return 1;
+    
+    }
+   return 0;
+}
 void generateSoudex(const char* input,char* soundexCode){
-   if (input[0] == '\0')
+   if (checkForEmptyInput)
     {
         soundexCode[0] = '\0';
     
